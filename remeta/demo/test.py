7832beaaa9cb6699973ccdadcb48f287d2bb7432
m-guggenmos <monaden@gmail.com>
2021-10-14 16:32:47 +0200

    Make compatible with pip

--- remeta/demo/test.py
+++
@@ -1,6 +0,0 @@
-import remeta
-import warnings
-warnings.filterwarnings('error')
-stimuli, choices, confidence = remeta.load_dataset('simple')  # load example dataset
-rem = remeta.ReMeta()
-rem.fit(stimuli, choices, confidence)
