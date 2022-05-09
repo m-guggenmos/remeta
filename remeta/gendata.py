59ecc38b1ae1f82530bda7c2ed3cfa3dbabde5d4
m-guggenmos <monaden@gmail.com>
2022-05-09 08:37:10 +0200

    Refactoring: new parameter naming scheme

--- remeta/gendata.py
+++ remeta/gendata.py
@@ -9,8 +9,8 @@ from .util import _check_param, TAB, type2roc
 
 
 class Simulation:
-    def __init__(self, nsubjects, nsamples, params, cfg, stimulus_ids, stimuli, choices, dv_sens, dv_sens_mode,
-                 dv_meta, confidence, noise_meta, likelihood_dist=None):
+    def __init__(self, nsubjects, nsamples, params, cfg, stimulus_ids, stimuli, choices, dv_sens_prenoise, dv_sens,
+                 dv_meta_prenoise, dv_meta, confidence_prenoise, confidence, noise_meta, likelihood_dist=None):
         self.nsubjects = nsubjects
         self.nsamples = nsamples
         self.params = params
@@ -21,9 +21,11 @@ class Simulation:
         self.stimuli = stimuli
         self.choices = choices
         self.correct = stimulus_ids == choices
+        self.dv_sens_prenoise = dv_sens_prenoise
         self.dv_sens = dv_sens
-        self.dv_sens_mode = dv_sens_mode
+        self.dv_meta_prenoise = dv_meta_prenoise
         self.dv_meta = dv_meta
+        self.confidence_prenoise = confidence_prenoise
         self.confidence = confidence
         self.noise_meta = noise_meta
         self.likelihood_dist = likelihood_dist
@@ -54,38 +56,34 @@ def simu_type1_responses(stimuli, params, cfg):
     thresh_sens = _check_param(params['thresh_sens'] if cfg.enable_thresh_sens else 0)
     bias_sens = _check_param(params['bias_sens'] if cfg.enable_bias_sens else 0)
 
-    dv_sens_before_noise = np.full(stimuli_final.shape, np.nan)
-    # dv_sens_before_noise[stimuli_final < 0] = (np.abs(stimuli_final[stimuli_final < 0]) > thresh_sens[0]) * \
-    #     (stimuli_final[stimuli_final < 0] - np.sign(stimuli_final[stimuli_final < 0]) * thresh_sens[0]) - bias_sens[0]
-    # dv_sens_before_noise[stimuli_final >= 0] = (np.abs(stimuli_final[stimuli_final >= 0]) > thresh_sens[1]) * \
-    #     (stimuli_final[stimuli_final >= 0] - np.sign(stimuli_final[stimuli_final >= 0]) * thresh_sens[1]) - bias_sens[1]
-    dv_sens_before_noise[stimuli_final < 0] = (np.abs(stimuli_final[stimuli_final < 0]) > thresh_sens[0]) * \
+    dv_sens_prenoise = np.full(stimuli_final.shape, np.nan)
+    dv_sens_prenoise[stimuli_final < 0] = (np.abs(stimuli_final[stimuli_final < 0]) > thresh_sens[0]) * \
         stimuli_final[stimuli_final < 0] - bias_sens[0]
-    dv_sens_before_noise[stimuli_final >= 0] = (np.abs(stimuli_final[stimuli_final >= 0]) > thresh_sens[1]) * \
+    dv_sens_prenoise[stimuli_final >= 0] = (np.abs(stimuli_final[stimuli_final >= 0]) > thresh_sens[1]) * \
         stimuli_final[stimuli_final >= 0] - bias_sens[1]
 
     if cfg.detection_model:
         nchannels = cfg.detection_model_nchannels  # number of sensory channels
-        p_active = np.tanh(np.abs(dv_sens_before_noise) / noise_sens)  # probability that a channel is active
-        active = np.repeat(p_active[:, :, None], nchannels, axis=2) > np.random.rand(*dv_sens_before_noise.shape,
+        p_active = np.tanh(np.abs(dv_sens_prenoise) / noise_sens)  # probability that a channel is active
+        active = np.repeat(p_active[:, :, None], nchannels, axis=2) > np.random.rand(*dv_sens_prenoise.shape,
                                                                                      nchannels)
         nactive = np.sum(active, axis=2)  # = k
-        correct = np.zeros(dv_sens_before_noise.shape)
-        correct[(nactive > 0) & (np.sign(dv_sens_before_noise) == np.sign(stimuli_final))] = 1
-        correct[(nactive > 0) & (np.sign(dv_sens_before_noise) != np.sign(stimuli_final))] = 0
+        correct = np.zeros(dv_sens_prenoise.shape)
+        correct[(nactive > 0) & (np.sign(dv_sens_prenoise) == np.sign(stimuli_final))] = 1
+        correct[(nactive > 0) & (np.sign(dv_sens_prenoise) != np.sign(stimuli_final))] = 0
         correct[nactive == 0] = np.random.randint(0, 2, np.sum(nactive == 0))
         stimulus_ids = (np.sign(stimuli) > 0).astype(int)
         choices = (correct == stimulus_ids).astype(int)
-        dv_sens_before_noise = p_active * nchannels
+        dv_sens_prenoise = p_active * nchannels
         dv_sens = nactive
         # if the decision value should be in original space, use this:
-        # dv_sens = np.sign(dv_sens_before_noise) * noise_transform_sens * \
+        # dv_sens = np.sign(dv_sens_prenoise) * noise_transform_sens * \
         #     np.arctanh(np.minimum(nchannels-1e-5, nactive) / nchannels)
     else:
-        dv_sens = dv_sens_before_noise + logistic_dist(scale=noise_sens * np.sqrt(3) / np.pi).rvs(size=stimuli.shape)
+        dv_sens = dv_sens_prenoise + logistic_dist(scale=noise_sens * np.sqrt(3) / np.pi).rvs(size=stimuli.shape)
         choices = (dv_sens >= 0).astype(int)
 
-    return choices, stimuli_final, dv_sens, dv_sens_before_noise
+    return choices, stimuli_final, dv_sens_prenoise, dv_sens
 
 
 def simu_data(nsubjects, nsamples, params, cfg=None, stimuli_ext=None, verbose=True, stimuli_stepsize=0.02,
@@ -110,7 +108,8 @@ def simu_data(nsubjects, nsamples, params, cfg=None, stimuli_ext=None, verbose=T
     for p in ('warping', 'thresh', 'bias', 'noise_transform'):
         if not getattr(cfg, f'enable_{p}_sens'):
             params.pop(f'{p}_sens', None)
-    for p in ('readout_term', 'noise_transform', 'confidence_term', 'scaling', 'slope'):
+    for p in ('noise_transform', 'evidence_bias_mult', 'evidence_bias_add', 'evidence_bias_mult_postnoise',
+              'confidence_bias_mult', 'confidence_bias_add'):
         if not getattr(cfg, f'enable_{p}_meta'):
             params.pop(f'{p}_meta', None)
     if not cfg.enable_noise_sens:
@@ -148,37 +147,47 @@ def simu_data(nsubjects, nsamples, params, cfg=None, stimuli_ext=None, verbose=T
         if stimuli_ext.shape != (nsubjects, nsamples):
             stimuli = np.tile(stimuli, (nsubjects, 1))
     stimulus_ids = (np.sign(stimuli) > 0).astype(int)
-    choices, stimuli_final, dv_sens, dv_sens_mode = simu_type1_responses(stimuli, params, cfg)
+    choices, stimuli_final, dv_sens_prenoise, dv_sens = simu_type1_responses(stimuli, params, cfg)
 
     if cfg.meta_noise_type is None:
-        dv_meta, confidence, noise_meta = None, None, None
+        dv_meta_prenoise, dv_meta, confidence_prenoise, confidence, noise_meta = None, None, None, None, None
     else:
-        if cfg.enable_readout_term_meta == 1:
-            dv_meta_before_noise = np.maximum(0, np.abs(dv_sens) + params['readout_term_meta'])
-        elif cfg.enable_readout_term_meta == 2:
-            dv_meta_before_noise = np.full(dv_sens.shape, np.nan)
+        if cfg.enable_evidence_bias_mult_meta == 1:
+            dv_meta_prenoise = params['evidence_bias_mult_meta'] * np.abs(dv_sens)
+        elif cfg.enable_evidence_bias_mult_meta == 2:
+            dv_meta_prenoise = np.full(dv_sens.shape, np.nan)
             neg, pos = dv_sens < 0, dv_sens >= 0
-            dv_meta_before_noise[neg] = np.maximum(0, np.abs(dv_sens[neg]) + params['readout_term_meta'][0])
-            dv_meta_before_noise[pos] = np.maximum(0, np.abs(dv_sens[pos]) + params['readout_term_meta'][1])
+            dv_meta_prenoise[neg] = params['evidence_bias_mult_meta'][0] * np.abs(dv_sens[neg])
+            dv_meta_prenoise[pos] = params['evidence_bias_mult_meta'][1] * np.abs(dv_sens[pos])
         else:
-            dv_meta_before_noise = np.maximum(0, np.abs(dv_sens))
+            dv_meta_prenoise = np.abs(dv_sens)
+
+        if cfg.enable_evidence_bias_add_meta == 1:
+            dv_meta_prenoise = np.maximum(0, dv_meta_prenoise + params['evidence_bias_add_meta'])
+        elif cfg.enable_evidence_bias_add_meta == 2:
+            dv_meta_prenoise = np.full(dv_sens.shape, np.nan)
+            neg, pos = dv_sens < 0, dv_sens >= 0
+            dv_meta_prenoise[neg] = np.maximum(0, dv_meta_prenoise[neg] + params['evidence_bias_add_meta'][0])
+            dv_meta_prenoise[pos] = np.maximum(0, dv_meta_prenoise[pos] + params['evidence_bias_add_meta'][1])
+
         if cfg.meta_noise_type == 'noisy_readout':
 
             if cfg.enable_noise_meta:
                 noise_meta = noise_meta_transform(
-                    dv_meta_before_noise, dv_sens=dv_sens,
+                    dv_meta_prenoise, dv_sens=dv_sens,
                     function_noise_transform_meta=cfg.function_noise_transform_meta, **params
                 )
             else:
                 noise_meta = cfg.noise_meta_default
-            dist = get_dist(cfg.meta_noise_model, mode=dv_meta_before_noise, scale=noise_meta,
+            dist = get_dist(cfg.meta_noise_model, mode=dv_meta_prenoise, scale=noise_meta,
                             meta_noise_type=cfg.meta_noise_type, lookup_table=lookup_table)  # noqa
 
             dv_meta = np.maximum(0, dist.rvs((nsubjects, nsamples)))
         else:
-            dv_meta = dv_meta_before_noise
+            dv_meta = dv_meta_prenoise
 
-        confidence = link_function(
+
+        confidence_prenoise = link_function(
             dv_meta=dv_meta, link_fun=cfg.meta_link_function, dv_sens=dv_sens, stimuli=stimuli_final,
             function_noise_transform_sens=cfg.function_noise_transform_sens,
             **params
@@ -187,8 +196,8 @@ def simu_data(nsubjects, nsamples, params, cfg=None, stimuli_ext=None, verbose=T
         if cfg.meta_noise_type == 'noisy_report':
             if cfg.enable_noise_meta:
                 noise_meta = noise_meta_transform(
-                    confidence, dv_sens=dv_sens, function_noise_transform_meta=cfg.function_noise_transform_meta,
-                    **params
+                    confidence_prenoise, dv_sens=dv_sens,
+                    function_noise_transform_meta=cfg.function_noise_transform_meta, **params
                 )
                 noise_meta = np.maximum(cfg.noise_meta_min, noise_meta)
             else:
@@ -200,23 +209,29 @@ def simu_data(nsubjects, nsamples, params, cfg=None, stimuli_ext=None, verbose=T
                                      f'noise_meta is 0.5 for metacognitive type {cfg.meta_noise_type} and noise model '
                                      f'{cfg.meta_noise_model}')
 
-            dist = get_dist(cfg.meta_noise_model, mode=confidence, scale=noise_meta,
+            dist = get_dist(cfg.meta_noise_model, mode=confidence_prenoise, scale=noise_meta,
                             meta_noise_type=cfg.meta_noise_type, lookup_table=lookup_table)
             confidence = np.maximum(0, np.minimum(1, dist.rvs((nsubjects, nsamples))))
+        else:
+            confidence = confidence_prenoise
 
     if squeeze:
         stimulus_ids = stimulus_ids.squeeze()
         stimuli = stimuli.squeeze()
         choices = choices.squeeze()
+        dv_sens_prenoise = dv_sens_prenoise.squeeze()
         dv_sens = dv_sens.squeeze()
+        dv_meta_prenoise = dv_meta_prenoise.squeeze()
         dv_meta = dv_meta.squeeze()
+        confidence_prenoise = confidence_prenoise.squeeze()
         confidence = confidence.squeeze()
 
     simulation = Simulation(
         nsubjects=nsubjects, nsamples=nsamples, params=params, cfg=cfg,
         stimulus_ids=stimulus_ids, stimuli=stimuli, choices=choices,
-        dv_sens=dv_sens, dv_sens_mode=dv_sens_mode, dv_meta=dv_meta,
-        confidence=confidence,
+        dv_sens_prenoise=dv_sens_prenoise, dv_sens=dv_sens,
+        dv_meta_prenoise=dv_meta_prenoise, dv_meta=dv_meta,
+        confidence_prenoise=confidence_prenoise, confidence=confidence,
         noise_meta=noise_meta  # noqa
     )
     if verbose:
@@ -239,9 +254,10 @@ if __name__ == '__main__':
         thresh_sens=0.2,
         bias_sens=0.2,
         noise_meta=0.2,
-        readout_term_meta=0
+        evidence_bias_add_meta=0
     )
-    options = dict(meta_noise_type='noisy_report', enable_thresh_sens=1, enable_bias_sens=1, enable_readout_term_meta=1)
+    options = dict(meta_noise_type='noisy_report', enable_thresh_sens=1, enable_bias_sens=1,
+                   enable_evidence_bias_add_meta=1)
     m = simu_data(1, 1000, params_simulation, **options)
     # model = reconstruct_observer(m.stimuli[0], m.choices[0], m.confidence[0], **{**options, **params_simulation})
     # plot_sensory_meta(model)
