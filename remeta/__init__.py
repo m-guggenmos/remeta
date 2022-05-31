9a031006bb1d61b75f9f06078fdac69f2766f7fc
m-guggenmos <monaden@gmail.com>
2022-05-31 15:16:00 +0200

    Update

--- remeta/__init__.py
+++ remeta/__init__.py
@@ -1,3 +1,4 @@
 from .model import *
 from .gendata import simu_data
-from .plot import plot_sensory_meta, plot_psychometric, plot_link_function, plot_confidence_dist, plot_confidence
+from .plot import plot_sensory_meta, plot_psychometric, plot_psychometric_sim, plot_link_function, \
+    plot_confidence_dist, plot_confidence, plot_confidence_sim
