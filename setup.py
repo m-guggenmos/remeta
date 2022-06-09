b8f2814006b57746e3da105c552e65ef3fb14f2a
m-guggenmos <monaden@gmail.com>
2022-06-09 11:10:46 +0200

    Remove package_data in favor of MANIFEST.in

--- setup.py
+++ setup.py
@@ -7,23 +7,34 @@ def read(fname):
 
 
 DESCRIPTION = (
-    "Reverse engineering of Metacognition toolbox"
+    'Reverse engineering of Metacognition toolbox'
 )
 
-DISTNAME = "remeta"
-MAINTAINER = "Matthias Guggenmos"
-MAINTAINER_EMAIL = "mg.corresponding@gmail.com"
-VERSION = "0.0.1"
-LICENCE = "MIT License"
+DISTNAME = 'remeta'
+MAINTAINER = 'Matthias Guggenmos'
+MAINTAINER_EMAIL = 'mg.corresponding@gmail.com'
+VERSION = '0.1.3'
+LICENCE = 'MIT License'
 
 INSTALL_REQUIRES = [
-    "numpy>=1.18.1",
-    "scipy>=1.3",
-    "multiprocessing_on_dill>=3.5.0a4",
-    "matplotlib>=3.1.3",
+    'numpy>=1.18.1',
+    'scipy>=1.3',
+    'multiprocessing_on_dill>=3.5.0a4',
+    'matplotlib>=3.1.3',
 ]
 
-PACKAGES = ["remeta"]
+PACKAGES = ['remeta']
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10'
+]
 
 try:
     from setuptools import setup
@@ -32,7 +43,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     setup(
         name=DISTNAME,
@@ -41,12 +52,12 @@ if __name__ == "__main__":
         maintainer=MAINTAINER,
         maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
-        long_description=open("README.md").read(),
-        long_description_content_type="text/x-rst",
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
         license=LICENCE,
         version=VERSION,
         install_requires=INSTALL_REQUIRES,
         include_package_data=True,
-        package_data={"remeta": ["data/*"]},
         packages=PACKAGES,
+        classifiers=CLASSIFIERS,
     )
