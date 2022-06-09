28580fdea8623e62506ab2d9ea4868300b9f38aa
m-guggenmos <monaden@gmail.com>
2022-06-09 09:01:51 +0200

    Avoid importing with *

--- remeta/__init__.py
+++ remeta/__init__.py
@@ -1,4 +1,5 @@
-from .model import *
+from .model import ReMeta, load_dataset
+from .configuration import Configuration
 from .gendata import simu_data
 from .plot import plot_sensory_meta, plot_psychometric, plot_psychometric_sim, plot_link_function, \
     plot_confidence_dist, plot_confidence, plot_confidence_sim
